a769f6c5f09931697e57c05a7be86936f5adcacc
Thomas Waldmann <tw AT waldmann-edv DOT de>
2014-06-28 16:57:32 +0200

    add license headers to xstatic code files

--- xstatic/__init__.py
+++ xstatic/__init__.py
@@ -1,1 +1,4 @@
+# Copyright: 2011-2014 by the XStatic authors, see AUTHORS.txt for details.
+# License: MIT license, see LICENSE.txt for details.
+
 __import__('pkg_resources').declare_namespace(__name__)
